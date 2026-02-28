492106abe51feeee0a0cb859000d5687d0277e6c
Itsurkan <itsurkan.001@gmail.com>
2026-02-28 10:43:01 +0200

    chore: remove unused MUI/Emotion/Popper deps, add vite-env.d.ts, fix unused import

--- src/app/pages/ImportPersonnel.tsx
+++ src/app/pages/ImportPersonnel.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useNavigate } from 'react-router';
 import { useLanguage } from '../context/LanguageContext';
 import { useImportPersonnel } from '../hooks/useImportPersonnel';
